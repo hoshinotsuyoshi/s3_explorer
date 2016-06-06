83b59e3368ba78c40aea8221783732e674b6f211
hoshinotsuyoshi <guitarpopnot330@gmail.com>
2016-06-07 00:46:23 +0900

    Add /buckets/:bucket/prefixes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  resources :buckets, only: [:show, :index]
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :buckets, only: [:show, :index] do
+    resources :prefixes, only: [:show, :index]
+  end
 end
