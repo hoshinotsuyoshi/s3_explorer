6599f91d7daf66fad7dda62d8ab1e9a80f29f08d
hoshinotsuyoshi <guitarpopnot330@gmail.com>
2016-06-07 00:22:00 +0900

    Fill spec/controllers/buckets_controller_spec.rb

---
+++ app/models/bucket.rb
@@ -0,0 +1,30 @@
+class Bucket
+  include ActiveModel::Model
+
+  def self.all
+    s3 = Aws::S3::Client.new
+    s3.list_buckets.buckets.map{|b| new(name: b.name)}
+  end
+
+  def self.find(name)
+    all.find{|b| b.name == name}
+  end
+
+  def initialize(name:)
+    @name = name
+  end
+
+  attr_reader :name
+
+  def ==(other)
+    if other.is_a? self.class
+      self.name == other.name
+    else
+      super
+    end
+  end
+
+  def to_param
+    name
+  end
+end
