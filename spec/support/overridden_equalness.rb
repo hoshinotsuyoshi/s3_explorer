7acd04421cf951afa5185bfb3b02d86ef990cd7e
hoshinotsuyoshi <guitarpopnot330@gmail.com>
2016-06-26 02:00:29 +0900

    Refactor

---
+++ spec/support/overridden_equalness.rb
@@ -0,0 +1,29 @@
+shared_examples_for 'overridden equalness' do |method:|
+    describe '#==' do
+    context 'given same class object' do
+      context "given same #{method}" do
+        it 'returns true' do
+          me = described_class.new(method => 'a')
+          other  = described_class.new(method => 'a')
+          expect(me == other).to be true
+        end
+      end
+
+      context "given different #{method}" do
+        it 'returns true' do
+          me = described_class.new(method => 'a')
+          other  = described_class.new(method => 'b')
+          expect(me == other).to be false
+        end
+      end
+    end
+
+    context 'given different class object' do
+      it 'returns false' do
+        me = described_class.new(method => 'a')
+        other  = 'a'
+        expect(me == other).to be false
+      end
+    end
+  end
+end
