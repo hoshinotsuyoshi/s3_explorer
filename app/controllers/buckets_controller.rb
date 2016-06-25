a46b56769ab5733a55627c53108a47221b8545b9
hoshinotsuyoshi <guitarpopnot330@gmail.com>
2016-06-26 01:23:09 +0900

    Add BucketsController#set_s3_objects

--- app/controllers/buckets_controller.rb
+++ app/controllers/buckets_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 class BucketsController < ApplicationController
   before_action :set_bucket, only: :show
+  before_action :set_s3_objects, only: :show
 
   # GET /buckets
   # GET /buckets.json
@@ -18,5 +19,10 @@ class BucketsController < ApplicationController
   # Use callbacks to share common setup or constraints between actions.
   def set_bucket
     @bucket = Bucket.find(params[:id])
+  end
+
+  def set_s3_objects
+    @files   = S3Object::File.select(bucket: params[:id], prefix: params[:prefix])
+    @folders = S3Object::Folder.select(bucket: params[:id], prefix: params[:prefix])
   end
 end
