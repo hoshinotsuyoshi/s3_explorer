dde364092d959910426c85ed57e7ed34a375d575
hoshinotsuyoshi <guitarpopnot330@gmail.com>
2016-06-25 15:54:51 +0900

    Add spec for Bucket#==

---
+++ spec/models/bucket_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe Bucket, type: :model do
+  describe '#==' do
+    context 'given same class object' do
+      context 'given same name' do
+        it 'returns true' do
+          bucket = Bucket.new(name: 'a')
+          other  = Bucket.new(name: 'a')
+          expect(bucket == other).to be true
+        end
+      end
+
+      context 'given different name' do
+        it 'returns true' do
+          bucket = Bucket.new(name: 'a')
+          other  = Bucket.new(name: 'b')
+          expect(bucket == other).to be false
+        end
+      end
+    end
+
+    context 'given different class object' do
+      it 'returns false' do
+        bucket = Bucket.new(name: 'a')
+        other  = 'a'
+        expect(bucket == other).to be false
+      end
+    end
+  end
+end
