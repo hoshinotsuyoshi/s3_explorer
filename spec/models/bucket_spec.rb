bb436e610ac56bc1b2be54af9f823fa4a199c682
hoshinotsuyoshi <guitarpopnot330@gmail.com>
2016-06-26 02:03:03 +0900

    require rails_helper

--- spec/models/bucket_spec.rb
+++ spec/models/bucket_spec.rb
@@ -1,5 +1,4 @@
 # frozen_string_literal: true
-require 'rails_helper'
 
 RSpec.describe Bucket, type: :model do
   it_behaves_like 'overridden equalness', method: :name
--- spec/models/s3_object/file_spec.rb
+++ spec/models/s3_object/file_spec.rb
@@ -1,6 +1,4 @@
 # frozen_string_literal: true
-require 'rails_helper'
-
 RSpec.describe S3Object::File, type: :model do
   describe '.select' do
     it 'returns object list' do
