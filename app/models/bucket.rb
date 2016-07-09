f5010e364ffde0b96498765156930e4c3f9cc85e
hoshinotsuyoshi <guitarpopnot330@gmail.com>
2016-07-09 11:13:41 +0900

    Fix

--- app/models/bucket.rb
+++ app/models/bucket.rb
@@ -4,15 +4,15 @@ class Bucket
 
   def self.all
     s3 = Aws::S3::Client.new
-    s3.list_buckets.buckets.map { |b| new(name: b.name) }
+    s3.list_buckets.buckets.map { |bucket| new(bucket) }
   end
 
   def self.find(name)
     all.find { |b| b.name == name }
   end
 
-  def initialize(name:)
-    @name = name
+  def initialize(bucket)
+    @name = bucket.name
   end
 
   attr_reader :name
