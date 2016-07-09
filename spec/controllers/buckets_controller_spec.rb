f86809b970d2fd74ff476c0382d54097defb6990
hoshinotsuyoshi <guitarpopnot330@gmail.com>
2016-07-09 17:15:17 +0900

    Improve BucketsController spec

--- spec/controllers/buckets_controller_spec.rb
+++ spec/controllers/buckets_controller_spec.rb
@@ -7,13 +7,15 @@ RSpec.describe BucketsController, type: :controller do
 
   let(:valid_session) { {} }
 
+  before do
+    s3 = Aws::S3::Client.new
+    s3.create_bucket(bucket: 'my-bucket')
+  end
+
+  after { FakeS3Server.restart }
+
   describe 'GET index' do
     it 'assigns all buckets as @buckets' do
-      Aws.config[:s3] = {
-        stub_responses: {
-          list_buckets: { buckets: [{ name: 'my-bucket' }] }
-        }
-      }
       bucket = Bucket.new valid_attributes
       get :index, params: valid_attributes, session: valid_session
       expect(assigns(:buckets)).to eq([bucket])
@@ -22,11 +24,6 @@ RSpec.describe BucketsController, type: :controller do
 
   describe 'GET show' do
     it 'assigns the requested bucket as @bucket' do
-      Aws.config[:s3] = {
-        stub_responses: {
-          list_buckets: { buckets: [{ name: 'my-bucket' }] }
-        }
-      }
       bucket = Bucket.new valid_attributes
       get :show, params: { id: bucket.to_param }, session: valid_session
       expect(assigns(:bucket)).to eq(bucket)
---
+++ spec/support/fake_s3_server.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+module FakeS3Server
+  class << self
+    def start
+      rootdir = Dir.mktmpdir
+
+      server  = Glint::Server.new(nil, signals: [:INT]) do |port|
+        exec "fakes3 -p #{port} -r #{rootdir}", out: '/dev/null', err: '/dev/null'
+        exit 0
+      end
+
+      server.start
+
+      Glint::Server.info[:fakes3] = {
+        address: "127.0.0.1:#{server.port}",
+        root: rootdir
+      }
+
+      Aws.config[:s3] = {
+        access_key_id: 'YOUR_ACCESS_KEY_ID',
+        secret_access_key: 'YOUR_SECRET_ACCESS_KEY',
+        endpoint: "http://#{Glint::Server.info[:fakes3][:address]}/",
+        force_path_style: true
+      }
+    end
+
+    def stop
+      rootdir = Glint::Server.info[:fakes3][:root]
+      FileUtils.remove_entry_secure(rootdir) if Dir.exist? rootdir
+    end
+
+    def restart
+      stop
+      start
+    end
+  end
+end
+
+RSpec.configure do |config|
+  config.before :suite do
+    FakeS3Server.start
+  end
+
+  config.after :suite do
+    FakeS3Server.stop
+  end
+end
