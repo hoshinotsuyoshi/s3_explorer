e147fecbbcfef4945d0690d0ed0b303adb8031a2
hoshinotsuyoshi <guitarpopnot330@gmail.com>
2016-07-09 19:28:05 +0900

    Refactor

--- spec/support/fake_s3_server.rb
+++ spec/support/fake_s3_server.rb
@@ -2,26 +2,10 @@
 module FakeS3Server
   class << self
     def start
-      rootdir = Dir.mktmpdir
-
-      server  = Glint::Server.new(nil, signals: [:INT]) do |port|
-        exec "fakes3 -p #{port} -r #{rootdir}", out: '/dev/null', err: '/dev/null'
-        exit 0
+      Dir.mktmpdir do |rootdir|
+        __start__(rootdir)
       end
-
-      server.start
-
-      Glint::Server.info[:fakes3] = {
-        address: "127.0.0.1:#{server.port}",
-        root: rootdir
-      }
-
-      Aws.config[:s3] = {
-        access_key_id: 'YOUR_ACCESS_KEY_ID',
-        secret_access_key: 'YOUR_SECRET_ACCESS_KEY',
-        endpoint: "http://#{Glint::Server.info[:fakes3][:address]}/",
-        force_path_style: true
-      }
+      set_dummy_s3_config
     end
 
     def stop
@@ -32,6 +16,32 @@ module FakeS3Server
     def restart
       stop
       start
+    end
+
+    private
+
+    def __start__(rootdir)
+      server = Glint::Server.new(nil, signals: [:INT]) do |port|
+        exec "fakes3 -p #{port} -r #{rootdir}",
+             out: '/dev/null', err: '/dev/null'
+        exit 0
+      end
+
+      server.start
+
+      Glint::Server.info[:fakes3] = {
+        address: "127.0.0.1:#{server.port}",
+        root: rootdir
+      }
+    end
+
+    def set_dummy_s3_config
+      Aws.config[:s3] = {
+        access_key_id: 'YOUR_ACCESS_KEY_ID',
+        secret_access_key: 'YOUR_SECRET_ACCESS_KEY',
+        endpoint: "http://#{Glint::Server.info[:fakes3][:address]}/",
+        force_path_style: true
+      }
     end
   end
 end
