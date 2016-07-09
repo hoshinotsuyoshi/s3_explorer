98d9e993f3a700d0bea1d0dca8b1d82a107dbd96
hoshinotsuyoshi <guitarpopnot330@gmail.com>
2016-07-09 22:18:17 +0900

    Improve feature spec

--- spec/features/main_spec.rb
+++ spec/features/main_spec.rb
@@ -1,15 +1,17 @@
 # frozen_string_literal: true
 describe 'main process', type: :feature, js: true do
   before do
-    create_s3_content(bucket: 'my-bucket.com', key: 'my-folder/my-file')
+    create_s3_content(bucket: 'my-bucket1.com', key: 'my-folder1/my-file1')
   end
 
-  it "html includes content 'Listing buckets' and 'my-bucket'" do
+  it "click bucket, folder, back-link" do
     visit '/buckets'
-    expect(page).to have_content 'Listing buckets'
-    expect(page).to have_content 'my-bucket.com'
 
-    click_on 'my-bucket'
+    click_on 'my-bucket1.com'
+
+    click_on 'my-folder1'
+
+    expect(page).to have_content 'my-file1'
 
     click_on 'Back'
   end
