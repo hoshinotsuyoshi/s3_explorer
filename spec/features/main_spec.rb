2e237f3321c3e53380d72205f7f5cf78fc6a181f
hoshinotsuyoshi <guitarpopnot330@gmail.com>
2016-07-09 12:31:51 +0900

    Add spec for feature

--- spec/features/main_spec.rb
+++ spec/features/main_spec.rb
@@ -1,7 +1,13 @@
 # frozen_string_literal: true
-describe 'the signin process', type: :feature do
-  it "html includes content 'List'" do
+describe 'main process', type: :feature, js: true do
+  it "html includes content 'Listing buckets' and 'my-bucket'" do
+    Aws.config[:s3] = {
+      stub_responses: {
+        list_buckets: { buckets: [{ name: 'my-bucket' }] }
+      }
+    }
     visit '/buckets'
-    expect(page).to have_content 'List'
+    expect(page).to have_content 'Listing buckets'
+    expect(page).to have_content 'my-bucket'
   end
 end
