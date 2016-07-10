3d39f17753fbd3c9a1838c6c1ba7522b1e979a9b
hoshinotsuyoshi <guitarpopnot330@gmail.com>
2016-07-10 23:48:09 +0900

    Improve feature spec

--- spec/features/main_spec.rb
+++ spec/features/main_spec.rb
@@ -1,7 +1,10 @@
 # frozen_string_literal: true
 describe 'main process', type: :feature, js: true do
   before do
-    create_s3_content(bucket: 'my-bucket1.com', key: 'my-folder1/my-file1')
+    create_s3_content(
+      bucket: 'my-bucket1.com',
+      key: 'my-folder1/my-folder2/my-file1'
+    )
   end
 
   it 'click bucket, folder, back-link' do
@@ -11,6 +14,12 @@ describe 'main process', type: :feature, js: true do
 
     click_on 'my-folder1'
 
+    click_on 'my-folder2'
+
     expect(page).to have_content 'my-file1'
+
+    click_on 'my-folder1'
+
+    expect(current_path).to eq('/buckets/my-bucket1.com/prefix/my-folder1%2F')
   end
 end
