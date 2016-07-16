6b136d5b0e86f8d2a1de9a47af3cc3c315a12456
hoshinotsuyoshi <guitarpopnot330@gmail.com>
2016-07-17 04:08:35 +0900

    Refactor matcher

--- spec/controllers/presigned_url_controller_spec.rb
+++ spec/controllers/presigned_url_controller_spec.rb
@@ -1,5 +1,13 @@
 # frozen_string_literal: true
 
+RSpec::Matchers.define :be_valid_s3_presigned_url do |path:|
+  match do |actual|
+    %r{\Ahttps?://}.match(actual) && \
+      URI(actual).path == path && \
+      actual.include?('X-Amz-Signature=')
+  end
+end
+
 RSpec.describe PresignedUrlController, type: :controller do
   let(:valid_session) { {} }
 
@@ -12,9 +20,8 @@ RSpec.describe PresignedUrlController, type: :controller do
       post :create,
            params: { bucket_id: 'my-bucket', prefix_id: 'my-folder/my-file' },
            session: valid_session
-      expect(assigns(:presigned_url)).to match %r{\Ahttps?://}
-      expect(URI(assigns(:presigned_url)).path).to eq '/my-bucket/my-folder/my-file'
-      expect(assigns(:presigned_url)).to include 'X-Amz-Signature='
+      expect(assigns(:presigned_url))
+        .to be_valid_s3_presigned_url(path: '/my-bucket/my-folder/my-file')
     end
   end
 end
