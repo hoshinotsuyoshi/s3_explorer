5e74501f70d68421a0c2e37f60d949e9438fef43
hoshinotsuyoshi <guitarpopnot330@gmail.com>
2016-07-18 02:11:58 +0900

    Add feature test

--- spec/features/main_spec.rb
+++ spec/features/main_spec.rb
@@ -20,6 +20,24 @@ describe 'main process', type: :feature, js: true do
 
     expect(page).to have_content 'my-file1'
 
+    expect(page).not_to have_content 'Pre-Signed'
+
+    find("button.btn.btn-default.btn-xs").click
+
+    sleep 0.1
+
+    expect(page).to have_content 'Pre-Signed URL'
+
+    click_on 'Fetch'
+
+    sleep 0.1
+
+    fetched_url = find('input.url').value
+    expect(fetched_url).to be_start_with('http://127.0.0.1')
+    expect(URI(fetched_url).path).to eq('/my-bucket1.com/my-folder1/my-folder2/my-file1')
+
+    click_on 'Close'
+
     click_on 'my-folder1'
 
     expect(current_path).to eq('/buckets/my-bucket1.com/prefix/my-folder1%2F')
