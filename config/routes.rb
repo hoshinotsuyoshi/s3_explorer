bb8fc81aa37051d7cbbf80ba21bb2fe859b13bbe
hoshinotsuyoshi <guitarpopnot330@gmail.com>
2016-07-18 01:31:35 +0900

    Refactor

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,12 @@ Rails.application.routes.draw do
   root to: 'top#show'
   resources :buckets, only: [:show, :index], id: %r{[^/]+} do
     get 'prefix/:prefix_id', to: 'prefix#show', as: :prefix
-    post 'prefix/:prefix_id/presigned_url/create', constraints: { prefix_id: /[^\/]+/ },
-                                                   to: 'presigned_url#create', as: :presigned_url_create
     get 'prefix/', to: 'prefix#show'
+    post(
+      'prefix/:prefix_id/presigned_url/create',
+      constraints: { prefix_id: %r{[^\/]+} },
+      to: 'presigned_url#create',
+      as: :presigned_url_create
+    )
   end
 end
