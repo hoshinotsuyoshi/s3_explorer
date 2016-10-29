a82a568f7efbb43ecb90f6465186d7374a36c569
hoshinotsuyoshi <guitarpopnot330@gmail.com>
2016-10-30 01:22:46 +0900

    Remove needless "#==" block

--- spec/support/overridden_equalness.rb
+++ spec/support/overridden_equalness.rb
@@ -1,31 +1,29 @@
 # frozen_string_literal: true
 
 shared_examples_for 'overridden equalness' do |method:|
-  describe '#==' do
-    context 'given same class object' do
-      context "given same #{method}" do
-        it 'returns true' do
-          me = described_class.new(method => 'a')
-          other = described_class.new(method => 'a')
-          expect(me == other).to be true
-        end
-      end
-
-      context "given different #{method}" do
-        it 'returns false' do
-          me = described_class.new(method => 'a')
-          other = described_class.new(method => 'b')
-          expect(me == other).to be false
-        end
+  context 'given same class object' do
+    context "given same #{method}" do
+      it 'returns true' do
+        me = described_class.new(method => 'a')
+        other = described_class.new(method => 'a')
+        expect(me == other).to be true
       end
     end
 
-    context 'given different class object' do
+    context "given different #{method}" do
       it 'returns false' do
         me = described_class.new(method => 'a')
-        other = 'a'
+        other = described_class.new(method => 'b')
         expect(me == other).to be false
       end
+    end
+  end
+
+  context 'given different class object' do
+    it 'returns false' do
+      me = described_class.new(method => 'a')
+      other = 'a'
+      expect(me == other).to be false
     end
   end
 end
